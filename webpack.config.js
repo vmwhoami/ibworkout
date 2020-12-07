23b3cf481efbe14a1b96fcf04d461ff4d1a7647a
vmwhoami <vmwhoami@gmail.com>
2020-12-07 16:54:09 +0200

    Add fonts styling to the navbar

--- webpack.config.js
+++ webpack.config.js
@@ -8,27 +8,26 @@ module.exports = {
   },
   module: {
     rules: [
+      // ...Other rules,
       {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
+          'style-loader',
           // Translates CSS into CommonJS
-          "css-loader",
+          'css-loader',
           // Compiles Sass to CSS
-          "sass-loader",
+          'sass-loader',
         ],
-      },
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: "file-loader"
+      }
+
     ],
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
-    ],
+
+
   },
+
+
 };
