f52b9156d76b71f2252931932fff68e27b6383e0
vmwhoami <vmwhoami@gmail.com>
2020-12-08 23:35:15 +0200

    Add event listener on scroll

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style/style.scss';
+
+
+const nav = document.querySelector('.nav');
+
+let navbarFade = () => {
+
+  let scroll = window.pageYOffset;
+  if (scroll > 20) {
+    nav.classList.add("faded")
+  } else {
+    nav.classList.remove("faded")
+  }
+}
+window.addEventListener('scroll', navbarFade)
+
+
+
