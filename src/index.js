4eba6cafb2a039dcd6d27c912fe6bad0b937520c
vmwhoami <vmwhoami@gmail.com>
2020-12-15 01:25:06 +0200

    add the checkbox

--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ import scrollfade from './components/navscroll';
 scrollfade()
 
 
-const phone = document.querySelector('.messanging__phone');
-const cover = document.querySelector('.messanging__cover');
-cover.addEventListener('click', e => {
-  const cover = e.target.parentElement;
+function messagingBtns() {
+  const container = document.querySelector('.messanging');
+  let cover = document.querySelector('.messanging__cover');
+  let phone = document.querySelector('.messanging__phone');
+  let messanger = document.querySelector('.messanging__messenger');
+  let whatsapp = document.querySelector('.messanging__whatsapp')
+  container.addEventListener('click', () => {
+    // cover.classList.add('hide');
+    // phone.classList.add('phone_tr')
+    // messanger.classList.add('messanger_tr')
+    // whatsapp.classList.add('whatsapp_tr')
+    // console.log(cover);
 
-  if (!cover.classList.contains('hide')) {
-    cover.classList.add('hide');
-    phone.classList.left = 0;
-    phone.classList.add('translate50');
-  }
+  });
 
-});
+
+}
+
+messagingBtns()
+
