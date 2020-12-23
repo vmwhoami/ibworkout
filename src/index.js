2c1d4a2f488cbb5da84035821605cd5ee0a8a51d
Vitalie Melnic <vmwhoami@gmail.com>
2020-12-23 16:21:14 +0200

    add checkbox.checked false on click

--- src/index.js
+++ src/index.js
@@ -3,23 +3,12 @@ import scrollfade from './components/navscroll';
 scrollfade()
 
 
-function messagingBtns() {
-  const container = document.querySelector('.messanging');
-  let cover = document.querySelector('.messanging__cover');
-  let phone = document.querySelector('.messanging__phone');
-  let messanger = document.querySelector('.messanging__messenger');
-  let whatsapp = document.querySelector('.messanging__whatsapp')
-  container.addEventListener('click', () => {
-    // cover.classList.add('hide');
-    // phone.classList.add('phone_tr')
-    // messanger.classList.add('messanger_tr')
-    // whatsapp.classList.add('whatsapp_tr')
-    // console.log(cover);
 
-  });
+let mobilenav = document.querySelectorAll('.navigation-item');
 
-
-}
-
-messagingBtns()
-
+mobilenav.forEach(el => {
+  el.addEventListener('click', () => {
+    let checkbox = document.querySelector('.navigation-checkbox')
+    checkbox.checked = false;
+  })
+})
