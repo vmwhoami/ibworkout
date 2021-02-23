6d3af6dd4b6b655d58aeef9ace3c27e8c718d03f
vmwhoami <vmwhoami@gmail.com>
2021-02-23 22:13:41 +0200

    add all loaders the app works without errors

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,24 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: "file-loader"
+      },
+      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: require.resolve('babel-loader'),
@@ -28,10 +46,7 @@ module.exports = {
         test: /\.(scss|css)$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
-      {
-        test: /\.png|svg|jpg|gif$/,
-        use: ['file-loader'],
-      },
+
     ],
   },
   devServer: {
