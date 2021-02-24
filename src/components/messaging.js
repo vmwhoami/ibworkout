1efd5bb353d00e444aa20ebd40250186b67751c3
vmwhoami <vmwhoami@gmail.com>
2021-02-24 22:45:42 +0200

    complete the styling of the message component

--- src/components/messaging.js
+++ src/components/messaging.js
@@ -1,44 +1,27 @@
 import React from 'react';
-import { MessageSvg, MessengerSvg } from '../components/Svgs'
+import { MessageSvg, MessengerSvg, WhatAppSvg, PhoneSvg, ViberSvg } from '../components/Svgs'
 
 
 const Messenging = () => {
   return (
     <div className="messanging">
       <input className="messanging__checkbox" type="checkbox" name="checkbox" id="toggle" />
-
       <label className="messanging__label" htmlFor="toggle">
         <a className="messanging__cover">
           <MessageSvg styling={"messanging__mesaj"} />
-          {/* <svg className="messanging__mesaj">
-            <use xlink:href="./sprite.svg#icon-mesaj"></use>
-          </svg> */}
         </a>
       </label>
-
       <a href="http://m.me/vmwhoami" target="blank" className="messanging__messenger shaper">
         <MessengerSvg styling={"messanging__svg"} />
-        {/* <svg className="messanging__svg">
-          <use xlink:href="./sprite.svg#icon-messenger"></use>
-        </svg> */}
       </a>
-
       <a href="tel:+37369136177" target="blank" className="messanging__phone shaper">
-        {/* <svg className="messanging__svg">
-          <use xlink:href="./sprite.svg#icon-phone"></use>
-        </svg> */}
+        <PhoneSvg styling={"messanging__svg"} />
       </a>
-
       <a href="http://wa.me/+37369136177" target="blank" className="messanging__whatsapp shaper">
-        {/* <svg className="messanging__svg fillgreen">
-          <use xlink:href="./sprite.svg#icon-whatsapp"></use>
-        </svg> */}
+        <WhatAppSvg styling={"messanging__svg fillgreen"} />
       </a>
-
       <a href="viber://chat?number=+37369136177" target="blank" className="messanging__viber shaper">
-        {/* <svg className="messanging__svg">
-          <use xlink:href="./sprite.svg#icon-viber"></use>
-        </svg> */}
+        <ViberSvg styling={"messanging__svg"} />
       </a>
     </div>
   )
