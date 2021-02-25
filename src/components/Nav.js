bdfa17c7b03bb1916a43fb5df40e53e68b9ee467
vmwhoami <vmwhoami@gmail.com>
2021-02-25 17:26:05 +0200

    add a fading effect to the navbar with useEffect and useState

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Logo, Instagram, Vk, FaceBk } from './Svgs'
 
 const Nav = () => {
+  const [fade, setFade] = useState(false)
+
+  const fadeOnScroll = () => {
+    if (window.scrollY > 200) {
+      setFade(true)
+    } else {
+      setFade(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", fadeOnScroll)
+    return () => {
+      window.removeEventListener("scroll", fadeOnScroll)
+    }
+  }, [])
+
   return (
-    <nav className="nav">
+    <nav className={fade ? "nav faded" : "nav"}>
       <MobileNav />
       <DescktopNav />
     </nav>
